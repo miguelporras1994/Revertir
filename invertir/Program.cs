a8e3310c324443b57e6a03090f7e44666729134c
Miguel Porras <porraangel@gmail.com>
2021-11-03 21:57:28 -0500

    invertir

---
+++ invertir/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace invertir
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+
+            string f1 = "", f2 = "";
+
+            Console.WriteLine("escriba la palabra a invertie :");
+            f1 = Console.ReadLine();
+
+            for ( int j = f1.Length - 1; j >= 0; j--) {
+
+                f2 += f1[j];
+
+                    }
+            Console.WriteLine("la frase invetidad es " + f2);
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
